3eb50e762d3b314c2e0087091f6eb479d5ca685c
JustinLiuCodes <justinliudesign@gmail.com>
2025-02-02 16:23:26 -0500

    Add beginning of having buttons for Category Navigation

--- src/components/CategoryNavigation.jsx
+++ src/components/CategoryNavigation.jsx
@@ -1,5 +1,13 @@
+import CategoryNavigationButton from './CategoryNavigationButton'
+
 const CategoryNavigation = () => {
-  return <nav>This is the navigation</nav>
+  return (
+    <nav>
+      <CategoryNavigationButton category="Cat 1" />
+      <CategoryNavigationButton category="Cat 2" />
+      <CategoryNavigationButton category="Cat 3" />
+    </nav>
+  )
 }
 
 export default CategoryNavigation
---
+++ src/components/CategoryNavigationButton.jsx
@@ -0,0 +1,5 @@
+const CategoryNavigationButton = ({ category }) => {
+  return <button key={category}>{category}</button>
+}
+
+export default CategoryNavigationButton
