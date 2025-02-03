1bdf7f064b042e475abd647789b23eb635936808
JustinLiuCodes <justinliudesign@gmail.com>
2025-02-03 17:54:47 -0500

    add hover to attraction card with description

--- src/components/AttractionCard.jsx
+++ src/components/AttractionCard.jsx
@@ -1,7 +1,11 @@
 const AttractionCard = ({ attraction }) => {
   return (
     <div className="attraction-card">
-      <img src={attraction.images[0]} alt={attraction.name} />
+      <div className="attraction-card-wrapper">
+        <img src={attraction.images[0]} alt={attraction.name} />
+        <div className="attraction-card-hover">{attraction.description}</div>
+      </div>
+
       <h3>{attraction.name}</h3>
     </div>
   )
