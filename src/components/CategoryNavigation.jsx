dd488c0ad38917d55f1b9af165d600917437d964
JustinLiuCodes <justinliudesign@gmail.com>
2025-02-04 12:18:43 -0500

    Add subtitle

--- src/components/CategoryNavigation.jsx
+++ src/components/CategoryNavigation.jsx
@@ -1,6 +1,11 @@
 const CategoryNavigation = ({ categories, setSelectedCategory }) => {
   return (
     <div>
+      <h3>Discover the beauty, culture, and adventure of Puerto Rico.</h3>
+      <p>
+        Explore its historic sites, stunning beaches, and outdoor wonders—all in
+        one place.
+      </p>
       {categories.map((category) => (
         <button key={category} onClick={() => setSelectedCategory(category)}>
           {category}
